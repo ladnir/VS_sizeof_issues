7cbb04397897eddb32f3becc3b89b812e64d9c3a
Peter Rindal <rindalp@oregonstate.edu>
2018-02-27 09:01:28 -0800

    bug

--- bug1.cpp
+++ bug1.cpp
@@ -1,5 +1,5 @@
 
-// simple version
+// sizeof inside aliased template
 template<class, class> struct A { };
 template<int> struct B { };
 
@@ -10,22 +10,3 @@ template<class T> using D = B<sizeof(A<T, int>)>;       // doesn't work
 
 
 
-
-
-
-
-// more noisy version
-#include <type_traits>
-#include <utility>
-#include <algorithm>
-
-template<typename U,
-    typename Enabled = typename std::enable_if<std::is_integral<U>::value>::type>
-    struct wrapper
-{
-};
-
-template<typename U, typename storage_type>
-using is_small = std::is_void<typename std::enable_if <
-        (sizeof(wrapper<U, char>) <= sizeof(storage_type))
-    >::type>;
--- bug2.cpp
+++ bug2.cpp
@@ -1,44 +1,43 @@
 #include <type_traits>
 
-namespace sb
+// sizeof interacting weirdly with virtual functions
+
+struct A
 {
-    struct A
-    {
-        virtual ~A() {};
-        virtual int* get() { return nullptr; }
-    };
+    virtual ~A() {};
+    virtual int* get() { return nullptr; }
+};
+
+template<typename U>
+struct B : A
+{
+    int* get2() { return &_u; }             // works with line 29
+    int* get() override { return &_u; }     // doesn't work with line 29
+    U _u;
+};
+
+template<typename T>
+struct C
+{
+    template<typename U>
+    void operator=(C<U>&& m)
+    {}
 
     template<typename U>
-    struct B : A
-    {
-        int* get2()  { return &_u; }            // works
-        int* get() override { return &_u; }     // doesn't work
-        U _u;
-    };
-
-    template<typename T>
-    struct C
-    {
-        template<typename U>
-        void operator=(C<U>&& m)
-        {}
-
-        template<typename U>
-        typename std::enable_if<
-            false &&
-            (sizeof(B<U>))           // doesnt work with line 15
-        >::type               
-            operator=(U&& u)
-        {}
-    };
-}
+    typename std::enable_if<
+        false &&
+        (sizeof(B<U>))           // doesnt work with line 15, appears to full instantiate B<U>.
+    >::type
+        operator=(U&& u)
+    {}
+};
 
 int main()
 {
 
-    sb::C<int> c1;
-    sb::C<int> c2;
-    
+    C<int> c1;
+    C<int> c2;
+
     c2 = std::move(c1);
 
     return 0;
